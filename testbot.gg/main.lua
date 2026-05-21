44e0ae7665c3f0203dde489f3fe9df763af0914e
waveringlizard9872 <sidiouspalpatine3@gmail.com>
2026-05-21 02:06:13 -0700

    Add files via upload

---
+++ testbot.gg/main.lua
@@ -0,0 +1,5 @@
+-- Kyanite UI
+
+local Repository = "https://raw.githubusercontent.com/waveringlizard9872/dragonnoob983423/refs/heads/main/testbot.gg";
+
+return loadstring(game:HttpGet(`{Repository}/framework/init.lua`))(Repository);
