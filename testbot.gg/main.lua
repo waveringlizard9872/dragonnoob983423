d3e23a913573ca3a5d2f4d5b3a6ced9bdca01964
waveringlizard9872 <sidiouspalpatine3@gmail.com>
2026-05-21 01:37:41 -0700

    Delete testbot.gg directory

--- testbot.gg/main.lua
+++
@@ -1,5 +0,0 @@
--- Kyanite UI
-
-local Repository = "https://raw.githubusercontent.com/waveringlizard9872/dragonnoob983423/refs/heads/main/testbot.gg";
-
-return loadstring(game:HttpGet(`{Repository}/framework/init.lua`))(Repository);
